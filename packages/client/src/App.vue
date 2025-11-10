69cd4e84db9a54437d6bf2aba9083315987c2cdd
alex1431999 <alexhaller99@yahoo.de>
2025-11-10 21:10:14 +0100

    feat: write to game window when user inputs something

--- packages/client/src/App.vue
+++ packages/client/src/App.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
 import UserInput from "@/components/UserInput.vue";
 import GameWindow from "@/components/GameWindow.vue";
+import {ref} from "vue";
+
+const messages = ref<string[]>([]);
 </script>
 
 <template>
-  <GameWindow :content="['hello', 'there']" />
-  <UserInput />
+  <GameWindow :content="messages" />
+  <UserInput @send="message => messages.push(message)" />
 </template>
